5aab06bec2f4098f09320b70df8e92efe2938e3e
Carlos Ruiz <18002171@alumnos.utleon.edu.mx>
2023-06-15 20:59:54 -0600

    JCRG 15/06/2023

---
+++ src/app/utl/iric/iric.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import { IAlumnosIric } from '../alumnos-iric';
+
+@Component({
+  selector: 'app-iric',
+  templateUrl: './iric.component.html',
+  styleUrls: ['./iric.component.css']
+})
+export class IricComponent {
+  imageWidth:number=50;
+  imageMargin:number=2;
+  muestraImg:boolean=true;
+  listFilter:string='sss';
+  showImage():void{
+    this.muestraImg=!this.muestraImg;
+  }
+  alumnosIric:IAlumnosIric[]=[
+{
+      matricula:1234,
+      nombre:'pedro',
+      edad:23,
+      correo:'pedro@gmail.com',
+      foto:'https://static.nationalgeographicla.com/files/styles/image_3200/public/nationalgeographic_2791022.webp?w=1150&h=647&p=righttop'
+    },
+    {
+      matricula:772,
+      nombre:'paulina',
+      edad:23,
+      correo:'paulina@gmail.com',
+      foto:'https://cdn0.ecologiaverde.com/es/posts/7/7/4/animales_que_viven_en_el_campo_3477_orig.jpg'
+    },
+    {
+      matricula:342,
+      nombre:'Dario',
+      edad:32,
+      correo:'dario@gmail.com',
+      foto:'https://okdiario.com/img/2020/02/09/jeff-griffith-zqypm8i60f8-unsplash1-655x368.jpg'
+    }
+  ]
+}
